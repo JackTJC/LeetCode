3dc1119b2ec255e11d19a42712085cf0d9db3fe6
JackTJC <tianjincai@hotmail.com>
2020-10-14 10:01:12 +0800

    finish #1253

---
+++ src/Array/ReconstructArray.java
@@ -0,0 +1,53 @@
+package Array;
+//#1253
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReconstructArray {
+    public static void main(String[] args) {
+        new Solution().reconstructMatrix(1,4,new int[]{2,1,2,0,0,2});
+    }
+    static class Solution {
+        public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
+            List<List<Integer>> result=new ArrayList<List<Integer>>();
+            int count=0;
+            List<Integer> up=new ArrayList<Integer>();
+            for(int i:colsum)
+            {
+
+                if(i==2)
+                {
+                    up.add(1);
+                    count++;
+                }
+                else
+                    up.add(0);
+            }
+            if(count>upper)
+                return new ArrayList<List<Integer>>();
+            for(int i=0;i<up.size();i++)
+            {
+                if(colsum[i]==1)
+                {
+                    if(count>=upper)
+                        break;
+                    up.set(i,1);
+                    count++;
+                }
+            }
+            result.add(up);
+            count=0;
+            List<Integer> low=new ArrayList<>();
+            for(int i=0;i<up.size();i++)
+            {
+                low.add(colsum[i]-up.get(i));
+                if(colsum[i]-up.get(i)==1)
+                    count++;
+            }
+            if(count!=lower)
+                return new ArrayList<List<Integer>>();
+            result.add(low);
+            return result;
+        }
+    }
+}
---
+++ src/String/RemoveAdjacentDuplicates.java
@@ -0,0 +1,30 @@
+package String;
+//#1209
+public class RemoveAdjacentDuplicates {
+    public static void main(String[] args) {
+        System.out.println(new Solution().removeDuplicates("yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy",4));
+    }
+    static class Solution {
+        public String removeDuplicates(String s, int k) {
+            for(int i=0;i<=s.length()-k;i++)
+            {
+                String subString = s.substring(i,i+k);
+                if(Solution.isDuplicate(subString))
+                {
+                    return new Solution().removeDuplicates(s.substring(0,i)+s.substring(i+k),k);
+                }
+            }
+            return s;
+        }
+        static boolean isDuplicate(String s)
+        {
+            char c=s.charAt(0);
+            for(char c_:s.toCharArray())
+            {
+                if(c!=c_)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
