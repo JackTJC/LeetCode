cc75a84c33c44a002506a7531aef312214d9f391
JackTJC <tianjincai@hotmail.com>
2020-10-15 17:06:24 +0800

    #817 finished

--- src/List/ListNode.java
+++ src/List/ListNode.java
@@ -6,4 +6,14 @@ public class ListNode {
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int [] v){
+        ListNode cur=this;
+        for(int i:v)
+        {
+            cur.val=i;
+            cur.next=new ListNode();
+            cur=cur.next;
+        }
+        cur.next=null;
+    }
 }
---
+++ src/Tree/ValidateTree.java
@@ -0,0 +1,34 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+//#1361
+public class ValidateTree {
+    public static void main(String[] args) {
+        new Solution().validateBinaryTreeNodes(4,new int[]{3,-1,1,-1},new int[]{-1,-1,0,-1});
+    }
+    static class Solution {
+        public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+            Integer i= 0;
+            List<Integer> count = new ArrayList<Integer>(1);
+            count.add(0);
+            transverse(0,leftChild,rightChild,count);
+            return count.get(0)==n;
+        }
+        void transverse(int root,int[] leftChild,int [] rightChild,List<Integer> count)
+        {
+            count.set(0,count.get(0)+1);
+            if(count.get(0)>leftChild.length)
+            {
+                count.set(0,leftChild.length+1);
+                return;
+            }
+            if(leftChild[root]!=-1)
+                transverse(leftChild[root],leftChild,rightChild,count);
+            if(rightChild[root]!=-1)
+                transverse(rightChild[root],leftChild,rightChild,count);
+        }
+    }
+}
