b1390453e278f9d9ef34cfbf876f7a2bb9fad57e
JackTJC <tianjincai@hotmail.com>
2020-12-29 10:30:25 +0800

    2020/12/29

--- BaseAlgorithm/ScheduleAlgorithm/LRUCache.py
+++ BaseAlgorithm/ScheduleAlgorithm/LRUCache.py
@@ -1,51 +1,88 @@
 
+# class LRUCache:
+#
+#     def __init__(self, capacity: int):
+#         self.cache={}
+#         self.counter={}
+#         self.capacity=capacity
+#         self.used=0
+#
+#
+#     def get(self, key: int) -> int:
+#         for k in self.counter.keys():
+#             self.counter[k]+=1
+#         if self.cache.get(key):
+#             #被访问则置0
+#             self.counter[key]=0
+#             return self.cache.get(key)
+#         else:
+#             return -1
+#
+#
+#
+#     def put(self, key: int, value: int) -> None:
+#         if self.used<self.capacity:
+#             self.cache[key]=value
+#             self.counter[key]=0
+#             self.used+=1
+#         else:
+#             #选择计数值最大的淘汰
+#             keys=list(self.counter.keys())
+#             x=lambda x:self.counter[x]
+#             keys.sort(key=x)
+#             del self.cache[keys[-1]]
+#             del self.counter[keys[-1]]
+#             #放入新值
+#             self.cache[key]=value
+#             self.counter[key]=0
+
+import time
+class Data:
+    def __init__(self,val):
+        self.val=val
+        self.time=time.time()
+    def getVal(self):
+        self.time=time.time()
+        return self.val
+    def update(self,val):
+        self.time=time.time()
+        self.val=val
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache={}
-        self.counter={}
+        self.cache=dict()
         self.capacity=capacity
-        self.used=0
-
 
     def get(self, key: int) -> int:
-        for k in self.counter.keys():
-            self.counter[k]+=1
-        if self.cache.get(key):
-            #被访问则置0
-            self.counter[key]=0
-            return self.cache.get(key)
+        ret=self.cache.get(key)
+        if ret:
+            return ret.getVal()
         else:
             return -1
 
-
-
     def put(self, key: int, value: int) -> None:
-        if self.used<self.capacity:
-            self.cache[key]=value
-            self.counter[key]=0
-            self.used+=1
+        if self.cache.get(key):
+            self.cache[key].update(value)
+            return
+        if len(self.cache)<self.capacity:
+            self.cache[key]=Data(value)
+            self.capacity+=1
         else:
-            #选择计数值最大的淘汰
-            keys=list(self.counter.keys())
-            x=lambda x:self.counter[x]
-            keys.sort(key=x)
-            del self.cache[keys[-1]]
-            del self.counter[keys[-1]]
-            #放入新值
-            self.cache[key]=value
-            self.counter[key]=0
-
-
+            k=list(self.cache.keys())
+            sortKey=lambda x:self.cache[x].time
+            k.sort(key=sortKey)
+            del self.cache[k[0]]
+            self.cache[key]=Data(value)
 
 
 if __name__ == '__main__':
     cache=LRUCache(2)
-
-    cache.get(2)
-    cache.put(2,6)
+    cache.put(1, 1)
+    cache.put(2, 2)
     cache.get(1)
-    cache.put(1,5)
-    cache.put(1,2)
-    cache.get(1)
+    cache.put(3, 3)
     cache.get(2)
+    cache.put(4, 4)
+    cache.get(1)
+    cache.get(3)
+    cache.get(4)
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Introduction
 一些经典的LeetCode题目,主要由python完成
 # Contents
-## [Array](Array)
+## [数组](Array)
 - [#75](Array/ColorClassify.py)颜色分类(荷兰国旗问题)
 - [#18](Array/FourSum.py) 四数之和
 - [#48](Array/RotateImage.py)旋转图片
-## [BackTrack](BackTrack)
+## [回溯算法](BackTrack)
 - [#39](BackTrack/Combination.py) 组合总和
 - [#interview 0804](BackTrack/PowerSet.py) 幂集
-## [BaseAlgorithm](BaseAlgorithm) (基础算法)
+## [基础算法](BaseAlgorithm) (基础算法)
 ### 图算法
 - [#207](BaseAlgorithm/Graph/IsDAG.py) 拓扑排序
 ### 调度算法
@@ -24,39 +24,40 @@
 ### 树算法
 - [后续遍历(非递归)](BaseAlgorithm/tree/postOrderTransverse.py)
 - [前缀树](BaseAlgorithm/tree/PrefixTree.py)
-## [BitOperation](BitOperation)
+## [位操作](BitOperation)
 - [#137](BitOperation/OnlyOneAppearance.py) 仅出现一次的数
-## [DFS](dfs)
+## [深度优先搜索](dfs)
 - [#200](dfs/NumOfIsland.py) 岛屿数量
 - [#79](dfs/SerachWord.py) 单词搜索
-## [DoublePointer](DoublePointer)
+## [双指针](DoublePointer)
 - [#27](DoublePointer/RemoveOneElement.py) 移除元素
-## [Dynamic Programing](DP)
+## [动态规划](DP)
+- [#300](DP/LIS.py) 最大上升子序列
 - [最长公共子序列](DP/MaxPublicSeq.py)
 - [最长公共子串](DP/MaxPublicSubstring.py)
 - [编辑距离](DP/EditDistance.py)
 - [#712](DP/leastAscii.py) 最小Ascii删除距离
 - [#5](DP/LongestPalindrome.py) 最长回文串
 - [#120](DP/triangle.py) 三角形最短路径和
-## [Greedy](Greedy)
+## [贪心算法](Greedy)
 - [#134](Greedy/CostAndGas.py) 油耗
 
-## [Hash Table](HashTable)
-## [LinkedList](LinkedList)
+## [哈希表](HashTable)
+## [链表](LinkedList)
 - [#206](LinkedList/Reverse.py)反转链表（两种解法）
 - [#206进阶](LinkedList/ReverseKList.py) K组反转链表
 - [#141](LinkedList/HasCycle.py) 链表是否有环
-## [Math](Math)
+## [数学](Math)
 - [#452](Math/MinArrow.py) 最少量的箭引爆气球
 - [#29](Math/Divide.py) 两数相除
 - [#43](Math/Multiply.py) 字符串相乘
-## [SlidingWindow](SlidingWindow)
+## [滑动窗口算法](SlidingWindow)
 - [#3](SlidingWindow/LongestSubstring.py) 无重复字符最长子串
-## [Stack](Stack)
+## [栈](Stack)
 - [#155](Stack/MinStack.py) 最小栈
 - [#155](Stack/MinStackSolution2.py) 最小栈优化解法
-## [String](String)
-## [Tree](Tree)
+## [字符串](String)
+## [树](Tree)
 - [#1080](Tree/ValidSearchTree.py) 验证二叉搜索树
 # Contact with me 
 E-mail:tianjincai@hotmail.com
