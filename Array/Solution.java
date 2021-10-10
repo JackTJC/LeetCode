7483110be12dd7cbd86cc31bc0376f98c018b2e3
ubuntu <ubuntu@localhost.localdomain>
2021-10-10 23:20:31 +0800

    update

--- Array/Solution.java
+++ Array/Solution.java
@@ -1,9 +1,16 @@
-package array;
+package Array;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class Solution {
+    public static void main(String[] args){
+        Solution o = new Solution();
+        int[] arr = new int[10];
+        System.out.println(o.longestArithSeqLength(arr));
+    }
     public int longestArithSeqLength(int[] nums) {
         Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -12,6 +19,10 @@ class Solution {
                 map.get(i).put(nums[j] - nums[i], j);
             }
         }
-        return 0
+        return 0;
+    }
+    public void testFunc(){
+        List<Object> l = new ArrayList<>();
+        l.add(1);
     }
 }
